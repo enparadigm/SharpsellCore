23cf61271a995e86b3fdc9e438eda68a84803833
Aman Verma <aman.verma@sharpsell.ai>
2025-07-10 15:09:30 +0530

    lic dev sdk beta release

--- artifacts/sqlite3.xcframework/ios-arm64_x86_64-simulator/sqlite3.framework/Headers/sqlite_cfg.h
+++ artifacts/sqlite3.xcframework/ios-arm64_x86_64-simulator/sqlite3.framework/Headers/sqlite_cfg.h
@@ -33,7 +33,7 @@
 #define HAVE_REALPATH 1
 #define HAVE_STDINT_H 1
 #define HAVE_STDLIB_H 1
-/* #undef HAVE_STRCHRNUL */
+#define HAVE_STRCHRNUL 1
 #define HAVE_STRINGS_H 1
 #define HAVE_STRING_H 1
 #define HAVE_SYS_STAT_H 1
