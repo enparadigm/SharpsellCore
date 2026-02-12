24a3364af990a1ed29d47e91a25642a1152b0487
vasu-sinojia <vasu.sinojia@sharpsell.ai>
2026-02-12 12:46:50 +0530

    feat: add flutter_local_notifications support and improve notification handling in SharpsellWrapper

--- Package.swift
+++ Package.swift
@@ -47,6 +47,7 @@ let package = Package(
                 "flutter_custom_tabs_ios",
                 "flutter_inappwebview_ios",
                 "flutter_js",
+                "flutter_local_notifications",
                 "flutter_native_contact_picker",
                 "flutter_web_auth_2",
                 
@@ -186,6 +187,9 @@ let package = Package(
         
             .binaryTarget(name: "flutter_js",
                           path: "artifacts/flutter_js.xcframework"),
+        
+            .binaryTarget(name: "flutter_local_notifications",
+                          path: "artifacts/flutter_local_notifications.xcframework"),
         
             .binaryTarget(name: "flutter_web_auth_2",
                           path: "artifacts/flutter_web_auth_2.xcframework"),
